0d39b1f83a5fe2ee7124f5823a27265045a7adbd
Joshua Campbell <joshcampbell191@gmail.com>
2025-10-12 09:30:10 -0400

    Updated GitHub Action

--- test_all.ps1
+++ test_all.ps1
@@ -51,7 +51,7 @@ $options = [ordered]@{
         Type = 'markdown'                                   #Report type: markdown or text
         Path = "$PSScriptRoot\Update-Force-Test-${n}.md"    #Path where to save the report
         Params= @{                                          #Report parameters:
-            Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
+            Github_UserRepo = $Env:gh_user_repo         #  Markdown: shows user info in upper right corner
             NoAppVeyor  = $true                             #  Markdown: do not show AppVeyor build shield
             Title       = "Update Force Test - Group ${n}"
             UserMessage = "[Ignored](#ignored) | [Update report](https://gist.github.com/$Env:gist_id)"       #  Markdown, Text: Custom user message to show
@@ -60,7 +60,7 @@ $options = [ordered]@{
 
     Gist = @{
         Id     = $Env:gist_id_test                          #Your gist id; leave empty for new private or anonymous gist
-        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+        ApiKey = $Env:gh_api_key                        #Your github api key - if empty anoymous gist is created
         Path   = "$PSScriptRoot\Update-Force-Test-${n}.md"  #List of files to add to the gist
         Description = "Update Force Test Report #powershell #chocolatey"
     }
